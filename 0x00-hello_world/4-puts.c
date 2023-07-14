8e9dafc030c8c8735bc7e24908ea7552e15460be
Tiago Patrick <santiago2091@gmail.com>
2023-07-14 08:01:52 -0700

    A simple c program to printout a string of words

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcc -xc - << EOF #include <stdio.h> int main() { puts("Programming is like building a multilingual puzzle"); return 0} EOF ./a.out
+gcc -xc - << EOF #include <stdio.h> int main() { puts("\"Programming is like building a multilingual puzzle"); return 0} EOF ./a.out
