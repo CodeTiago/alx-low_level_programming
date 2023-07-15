9d9162b0d386a5e80f9fe05357413cb80129dd3e
Tiago Patrick <santiago2091@gmail.com>
2023-07-15 07:12:18 -0700

    Simple prog to display exactly with proper grammar, but the outcome is a piece of art,

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-/**
- * int main - Entry point
- *
- * Return: Always zero if executed succesfully 
- */
 
-int main()
-{ 
-	printf("with proper grammar, but the outcome is a piece of art,");
+/**
+ * main function - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: zero if executed properly
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
