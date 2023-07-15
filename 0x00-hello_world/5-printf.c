8dd7847f96f5c9b5fc93e07891b9fb28b6711836
Tiago Patrick <santiago2091@gmail.com>
2023-07-15 06:31:43 -0700

    A simple program that displays exactly, with proper grammar, but the outcome is a piece of art,

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,12 @@
-#include <stdio.h> int main(void) {
-printf("with proper grammar, but the outcome is a piece of art,"); return 0; }
+#include <stdio.h>
+/* The simple c program to print a string of words
+ * int main() is the function that allow the prog to output or input
+ * We make sure we include  #include <stdio.h> from its library for each c program
+ *
+ * */
+int main(void)
+{
+         printf("with proper grammar, but the outcome is a piece of art,"); 
+	 
+	 return (0);
+}
