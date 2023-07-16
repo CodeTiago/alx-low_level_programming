4ea4cfcc353521fa2126a169609334c7e80d0269
Tiago Patrick <santiago2091@gmail.com>
2023-07-16 10:55:44 -0700

    A simple program that printsout alphabets in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main function: it gets read to begin the program execution
+ *main: reads the input from the key or output the result
+ *
+ * the loop execute alphabets in lowercase once the condition is met
+ * retun 0: if the conndition is either met whether not
+ */
+int main()
+{ char = letter;
+	for(letter = 'a'; lettr <= 'z'; letter++)
+	{
+	   putchar(letter);
+	}
+	putchar("\n");
+
+	return(0);
+
+}
