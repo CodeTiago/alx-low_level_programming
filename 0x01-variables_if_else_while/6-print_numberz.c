7bdb05f9037fa49a914f13ba94348c8edbf981c3
Tiago Patrick <santiago2091@gmail.com>
2023-07-17 07:07:18 -0700

    a program that prints all single digit numbers of base 10 starting from 0, but don't use data type char and function printf,nor  puts

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Prints all single digit numbers of base 10
+ * main - Prints all single digit numbers of base 10 starting from 0,
+ *        only using putchar and without char variables.
  *
- *Return - 0 if succeed otherwise terminate
+ * Return: Always 0.
  */
 int main(void)
 {
