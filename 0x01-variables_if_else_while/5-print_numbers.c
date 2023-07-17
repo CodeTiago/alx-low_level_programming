f461581d3dcefa5ec025871e3e6c938d37b88b7f
Tiago Patrick <santiago2091@gmail.com>
2023-07-17 05:40:17 -0700

    a program that prints the alphabet in lowercase,except e and q, followed by a new line

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - Prints numbers.
+ * main - Prints numbers from 0 to 9.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 if succeed
  */
 int main(void)
 {
