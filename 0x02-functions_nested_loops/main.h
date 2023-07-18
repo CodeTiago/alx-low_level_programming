66d72a6d2c823f30d31755937268a3338e53a5cb
Tiago Patrick <santiago2091@gmail.com>
2023-07-18 07:45:14 -0700

    Just added few explaination on the FUNCTION PROTOTYPES

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,15 +5,71 @@
 */
 
 void print_alphabet(void);
+/**
+ * This is the function prototype for a function named print_alphabet.
+ * It indicates that the function does not take any parameters (void)
+ * and does not return any value (void).
+ */
 void jack_bauer(void);
+/**
+ * This function prototrype does not take any parameters (void)
+ *  and does not return any value (void).
+ *
+ */
 void print_alphabet_x10(void);
+/**
+ * Like the previous prototypes, it does not take any parameters (void)
+ *   and does not return any value (void).
+ */
 void times_table();
+/**
+ *  It does not specify whether it takes any parameters or returns a value.
+ * The absence of (void) or other parameter types suggests that the function's
+ * declaration is incomplete or not properly defined.
+ */
 void print_to_98(int);
+/**
+ * It indicates that the function takes an integer parameter (int) 
+ * and does not return any value (void).
+ */
 void print_times_table(int);
+/**
+ * It specifies that the function takes an integer parameter (int) and does
+ *  not return any value (void).
+ */
 int _putchar(char);
+/**
+ * It indicates that the function takes a character parameter (char) and returns
+ * an integer value (int).
+ *
+ */
 int _islower(int c);
+/**
+ * It specifies that the function takes an integer parameter (int) and returns an
+ *  integer value (int).
+ */
 int _isalpha(int c);
+/**
+ * It indicates that the function takes an integer parameter (int) and returns
+ * an integer value (int).
+ */
 int print_sign(int n);
+/**
+ *  It specifies that the function takes an integer parameter (int) and returns
+ *  an integer value (int).
+ * */
 int _abs(int);
+/**
+ * It indicates that the function takes an integer parameter (int) and returns an
+ *  integer value (int).
+ */
 int add(int n, int m);
+/**
+ * It specifies that the function takes two integer parameters (int n and int m) and
+ * returns an integer value (int).
+ */
 int print_last_digit(int n);
+/**
+ * the function prototype for a function named print_last_digit indicates that the function
+ * takes an integer parameter (int) and returns an integer value (int).
+ */
