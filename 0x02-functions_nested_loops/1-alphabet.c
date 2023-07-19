67b0e6d32a6690844e732b27f5912de55c47b4e0
Tiago Patrick <santiago2091@gmail.com>
2023-07-19 04:54:40 -0700

    a function that prints the alphabet, in lowercase, followed by a new line.

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,10 +3,11 @@
  * print_alphabet - This is an entry point
  *
  * Return: 0 if succeed otherwise terminate
- * */
+ */
 void print_alphabet(void)
 {
 	char tiago = 'a';
+	
 	while (tiago <= 'z')
 	{
 		_putchar(tiago);
