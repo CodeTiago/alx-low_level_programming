1b03a636d6ccc4bca4e8e49cdfd9c702db529180
Tiago Patrick <santiago2091@gmail.com>
2023-07-19 05:27:30 -0700

    a function prototype that checks for lowercase character.

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 /**
  * _islower - this function prototype checks for lowercase character
  * Return: 1 if it's lowercase 0 otherwise
- * c - character in the parameter for checking
+ * @c: character in the parameter for checking
  */
 int _islower(int c)
 {
