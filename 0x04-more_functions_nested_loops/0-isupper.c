7c2f2072ffe0f06fedb785fdd4d1e3f47396fd5f
Tiago Patrick <santiago2091@gmail.com>
2023-07-21 07:20:11 -0700

    A program using a function prototype _isupper to determine the uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isupper - it checks for uppercase character
+ * @c: the character that needs to be checked
+ * Return: 1 if its uppercase 0 otherwise+
+ */
+int _isupper(int c)
+{
+	return(c >= 'A' && c <= 'Z');
+}
