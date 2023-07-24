1ce98cfc55365a6070c10cad2ef78acbbc941ad7
Tiago Patrick <santiago2091@gmail.com>
2023-07-24 04:53:44 -0700

    a program that finds and prints the sum of the even-valued terms whose values do not exceed 4,000,000 in the Fibonacci sequence

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,22 +7,24 @@
  */
 int main(void)
 {
-	unsigned long int i, j, k, next;
-	long int sum = 0;
+	int i;
+	unsigned long int j, k, next, sum;
+
 	j = 1;
 	k = 2;
-       
-	while (j <= 4000000)
+	sum = 0;
+
+	for (i = 1; i <= 33; ++i)
 	{
-		if (j % 2 == 0)
+		if (j < 4000000 && (j % 2) == 0)
 		{
-			sum += j;
+			sum = sum + j;
 		}
 		next = j + k;
 		j = k;
 		k = next;
 	}
-	printf("%ld\n", sum);
+
+	printf("%lu\n", sum);
 
 	return (0);
-}
