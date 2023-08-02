159a71a2ca2bdb39a4811409ed0a60dafdefb2a0
Tiago Patrick <santiago2091@gmail.com>
2023-08-02 05:54:32 -0700

    a function that returns the natural square root of a number.

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -23,22 +23,6 @@ int power_operation(int n, int c)
  * @n: input number.
  * Return: natural square root.
  */
-int power_operation(int n, int c)
-{
-	if (c % (n / c) == 0)
-	{
-		if (c * (n / c) == n)
-			return (c);
-		else
-			return (-1);
-	}
-	return (0 + power_operation(n, c + 1));
-}
-/**
- * _sqrt_recursion - returns the natural square root of a number.
- * @n: input number.
- * Return: natural square root.
- */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
